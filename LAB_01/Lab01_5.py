168e88f1c995a2d6550699ad236d47239fd87e44
Mateusz Warzecha PC <warzecha.mati@gmail.com>
2022-03-15 21:46:52 +0100

    anti multiplication of moves halfway done

--- LAB_01/Lab01_5.py
+++ LAB_01/Lab01_5.py
@@ -1,8 +1,6 @@
-from ctypes import sizeof
-
-
-player = 1
+player = 0
 p = 0
+previous_moves =[]
 global possition
 possition = [1 , 2 , 3, 4 ,5 ,6 ,7 ,8 ,9]
 
@@ -42,10 +40,7 @@ def plansza_down(a, r):
 def ruch(x):
     global p
     global player
-    if player%2==1:
-        p=1         #gracz 1
-    else:
-        p=2         #gracz 2
+    p=player%2 + 1
     plansza_up(x, p)
     plansza_mid(x, p)
     plansza_down(x, p)
@@ -60,44 +55,48 @@ def pusta_plansza():
 
 pusta_plansza()
 
-move = input("gdzie?   ")
+#move = input("gdzie?   ")
 
-ruch(int(move))
+#ruch(int(move))
 
 gamestatus=1
 
 while gamestatus == 1:
     if possition[0]==possition[4]==possition[8]:
         print("gracz "+ str(p) +" wygrywa !!!!!")
-        possition = [1 , 2 , 3, 4 ,5 ,6 ,7 ,8 ,9]
         gamestatus=0
-        break
     else:
         if possition[2]==possition[4]==possition[6]:
             print("gracz "+ str(p) +" wygrywa !!!!!")
-            possition = [1 , 2 , 3, 4 ,5 ,6 ,7 ,8 ,9]
             gamestatus=0
-            break
         else:
             for i in range(0, 3):
                 if possition[0+3*i]==possition[1+3*i]==possition[2+3*i]:
                     print("gracz "+ str(p) +" wygrywa !!!!!")
-                    possition = [1 , 2 , 3, 4 ,5 ,6 ,7 ,8 ,9]
                     gamestatus=0
                     break
                 else:
                     if possition[0+i]==possition[3+i]==possition[6+i]:
                         print("gracz "+ str(p) +" wygrywa !!!!!")
-                        possition = [1 , 2 , 3, 4 ,5 ,6 ,7 ,8 ,9]
                         gamestatus=0
                         break
             else:
-                move = input("RUCH GRACZA "+ str(p+1)+ ", GDZIE IDZIESZ?")
+                move = input("RUCH GRACZA "+ str(player%2 + 1)+ ", GDZIE IDZIESZ?")
+                previous_moves.append(move)
+                for z in range(len(previous_moves)):
+                    for q in range(len(previous_moves)):
+                        if previous_moves[z] == previous_moves[q] and z != q:
+                            previous_moves.remove(move)
+                            move = input("TO POLE JEST JUZ ZAJETE, wybierz inne ")
                 ruch(int(move))
     if gamestatus==0:   
         rewanz = input("Grasz nexta?\n type: 'yes' or 'no'" )
         if rewanz == "yes":
+            possition = [1 , 2 , 3, 4 ,5 ,6 ,7 ,8 ,9]
             gamestatus=1
+            player = 0
+            p=0
+            pusta_plansza()
         else:
             if rewanz == "no":
                 print("Thank You for playing")
