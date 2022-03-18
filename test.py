4be046811892ca94d270ebcbc67de2a35271f738
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-03-18 23:20:38 +0100

    added lab instructions

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-a = 5
-b = 10
-if a == 10:
-    print("tak")
-else:
-    if a ==7:
-        print("tak")
+import random
+for i in range(10):
+    move =  random.randint(1, 9)
+    print(type(move))
+
+k=input()
+print(type(k))
+print("#################### \n#   TIC TAC TOE   #\n####################")
