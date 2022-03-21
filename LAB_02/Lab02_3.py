12bb3a5d8b4b12921f7f6cd9370e38cea6d6dd88
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-03-21 16:24:17 +0100

    task no 3 fully done

--- LAB_02/Lab02_3.py
+++ LAB_02/Lab02_3.py
@@ -1,15 +1,19 @@
+from encodings import utf_8
 import time
 
 
-txt = input("Dowolny teks: ")
-
+txt = input("Insert text: ")
 word_count = len(txt.split())
-print(word_count)
-
 txt = txt.lower()
-
-print(txt)
-
 start=time.time()
+
+dictionary = open('SJP.txt' ,encoding='utf_8' ).read().splitlines()
+if word_count==1:
+    if txt in dictionary:
+        print("Given word exists in polish dictionary")
+    else:
+        print("Given word does not exist in polish dictionary")
+else:
+    print("You have given "+str(word_count)+ " words.")
 
 print(time.time()-start)
