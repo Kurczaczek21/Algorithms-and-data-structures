4cf8e11a79aee54d54def45becb7a11efde50e45
Mateusz Warzecha PC <warzecha.mati@gmail.com>
2022-03-23 22:05:27 +0100

    last task almost done

--- LAB_02/Lab02_2.py
+++ LAB_02/Lab02_2.py
@@ -14,9 +14,25 @@ for row in file:
 
 new_file = [sub.split(', ',1) for subl in new_file for sub in subl]
 
-
 new_file=sorted(new_file, key=itemgetter(0))
 
-for i in range(10):
-    print(new_file[i][0])
+alreadyUsed = []
 
+for i in range(len(new_file)):
+    if int(new_file[i][0]) in alreadyUsed:
+        new_file[i][0]=int(new_file[i-1][0])+1
+    alreadyUsed.append(int(new_file[i][0]))
+
+deleted_words=[]
+current_file_length=len(new_file)
+possition=0
+while possition < current_file_length:
+    new_file[possition][1]=new_file[possition][1].split(' ')
+    for j in range(len(new_file[possition][1])):
+        if len(new_file[possition][1][j])>1:
+            if ord(new_file[possition][1][j][0])==(ord(new_file[possition][1][j][1])+1) or ord(new_file[possition][1][j][0])==(ord(new_file[possition][1][j][1])-1):
+                deleted_words.append(str(new_file[possition][0])+"  "+str(new_file[possition][1][j]))
+                # current_file_length-=1
+                # del new_file[possition][1][j]
+            possition+=1
+print(deleted_words)
