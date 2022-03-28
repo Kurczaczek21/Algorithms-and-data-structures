917f328738af34471b1946ba99e7b2480402ac5a
Mateusz Warzecha PC <warzecha.mati@gmail.com>
2022-03-28 22:18:33 +0200

    permission to move files denied

--- LAB_03/Lab03_1.py
+++ LAB_03/Lab03_1.py
@@ -5,9 +5,23 @@ import glob, os
 
 files = glob.glob('LAB_03/zadanie1/*')
 
-for line in files:
-    print(type(line))
-    dog=line.split('\\')[1]
-    print("__________"+dog)
+for i in range(len(files)):
+    files[i]=files[i].replace('LAB_03/zadanie1\\','')
+    i+=1
+# for i in range(26):
+#     new_folder=(chr(65+i))
+#     os.mkdir(os.path.join('E:\PROGRAMMS\Collage-AiSD\LAB_03\SORTED FOLDERS',new_folder ))
 
-print(files)
+sorted=glob.glob('LAB_03/SORTED FOLDERS/*')
+
+for i in range(len(sorted)):
+    sorted[i]=sorted[i].replace('LAB_03/SORTED FOLDERS\\','')
+    i+=1
+
+for j in range(len(files)):
+    for i in range(len(sorted)):
+        if sorted[i]==files[j][0]:
+            source=files[j]
+            dest=sorted[i]
+            os.replace('E:\PROGRAMMS\Collage-AiSD\LAB_03\zadanie1\\'+ source , "E:\PROGRAMMS\Collage-AiSD\LAB_03\SORTED FOLDERS\\"+dest)
+    
