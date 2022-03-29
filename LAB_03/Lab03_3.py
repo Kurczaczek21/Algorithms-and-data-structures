48537d9b0d2d919edd9895ee2dff43a90d804000
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-03-29 16:06:47 +0200

    sth better

--- LAB_03/Lab03_3.py
+++ LAB_03/Lab03_3.py
@@ -1,8 +1,22 @@
-from math import sin
-import random
+from math import sin, sqrt
+from random import randint
 
-def integral_circle(r):
+def integral_circle(r, accuracy):
     background=r*r*4
     area=3.14*r*r
-    if y 
+    x_list=[]
+    y_list=[]
+    for i in range(accuracy):
+        x_list.append(randint(0,r))
+        y_list.append(randint(0,r))
+        if y_list[i]<sqrt(r*r-(x_list[i]-r)*(x_list[i]-r))+r or y_list[i]>sqrt(r*r-(x_list[i]-r)*(x_list[i]-r))+r:
+            print("pepaTAS")
 
+        i+=1
+
+    print(background)
+    print(area)
+    print(x_list)
+    print(y_list)
+
+integral_circle(4,100)
