dfb0999c48ad141f0e06aef2f13f3799d69f3d4c
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-03-29 13:24:19 +0200

    task no 1 done again :)

--- LAB_03/Lab03_1.py
+++ LAB_03/Lab03_1.py
@@ -1,27 +1,15 @@
 import glob, os
 
-
-# print(os.listdir('./LAB_03/zadanie1'))
-
 files = glob.glob('LAB_03/zadanie1/*')
 
 for i in range(len(files)):
     files[i]=files[i].replace('LAB_03/zadanie1\\','')
     i+=1
-for i in range(26):
-    new_folder=(chr(65+i))
-    os.mkdir(os.path.join('E:\PROGRAMMS\Collage-AiSD\LAB_03\SORTED FOLDERS',new_folder ))
 
-sorted=glob.glob('LAB_03/SORTED FOLDERS/*')
 
-for i in range(len(sorted)):
-    sorted[i]=sorted[i].replace('LAB_03/SORTED FOLDERS\\','')
-    i+=1
-
-for j in range(len(files)):
-    for i in range(len(sorted)):
-        if sorted[i]==files[j][0]:
-            source=files[j]
-            dest=sorted[i]
-            os.replace('E:\PROGRAMMS\Collage-AiSD\LAB_03\zadanie1\\'+ source , "E:\PROGRAMMS\Collage-AiSD\LAB_03\SORTED FOLDERS\\"+dest+'\\'+source)
-    
+for i in range(len(files)):
+    try:
+        os.mkdir('C:\PY prog\LAB_03\zadanie1\\'+files[i][0])
+    except FileExistsError:
+        pass
+    os.replace('C:\PY prog\LAB_03\zadanie1\\'+ files[i] , "C:\PY prog\LAB_03\zadanie1\\"+files[i][0]+ '\\'+ files[i])
