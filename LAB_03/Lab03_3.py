7f385d40bcf8662a66fff420d3579b62c928c8b5
Mateusz Warzecha PC <warzecha.mati@gmail.com>
2022-04-01 01:52:18 +0200

    integralls fully done

--- LAB_03/Lab03_3.py
+++ LAB_03/Lab03_3.py
@@ -3,30 +3,22 @@ import random
 
 def integral_circle(r, accuracy):
     background=r*r*4
-    x_list=[]
-    y_list=[]
     counter=0
     for i in range(accuracy):
-        x_list.append(random.uniform(0,2*r))
-        y_list.append(random.uniform(0,2*r))
-        if y_list[i]<sqrt(r*r-(x_list[i]-r)*(x_list[i]-r))+r and y_list[i]>-1*(sqrt(r*r-(x_list[i]-r)*(x_list[i]-r)))+r:
+        x=random.uniform(0,2*r)
+        y=random.uniform(0,2*r)
+        if y<=sqrt(r*r-(x-r)*(x-r))+r and y>=-1*(sqrt(r*r-(x-r)*(x-r)))+r:
             counter+=1
-        i+=1
-    
     return (counter/accuracy)*background
 
-print(integral_circle(4,100000))
-
 def integral_sin(accuracy):
-    x_list=[]
-    y_list=[]
     counter=0
     for i in range(accuracy):
-        x_list.append(random.uniform(0,2))
-        y_list.append(random.uniform(0,1))
-        if y_list[i]<sin(x_list[i]):
+        x=random.uniform(0,2)
+        y=random.uniform(0,1)
+        if y<=sin(x):
             counter+=1
-        i+=1
     return (counter/accuracy)*2
 
-print(integral_sin(10000))
+print(integral_circle(4,10000000))
+print(integral_sin(1000000))
