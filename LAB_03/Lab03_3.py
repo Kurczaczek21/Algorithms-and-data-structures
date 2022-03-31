978cc858b5e16e1a5a8d93213ac1d9f25ff1a14b
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-03-31 19:13:25 +0200

    taks 3.1 done

--- LAB_03/Lab03_3.py
+++ LAB_03/Lab03_3.py
@@ -1,22 +1,24 @@
 from math import sin, sqrt
-from random import randint
+import random
 
 def integral_circle(r, accuracy):
     background=r*r*4
-    area=3.14*r*r
+    area=3.141592*r*r
     x_list=[]
     y_list=[]
+    counter=0
     for i in range(accuracy):
-        x_list.append(randint(0,r))
-        y_list.append(randint(0,r))
-        if y_list[i]<sqrt(r*r-(x_list[i]-r)*(x_list[i]-r))+r or y_list[i]>sqrt(r*r-(x_list[i]-r)*(x_list[i]-r))+r:
-            print("pepaTAS")
-
+        x_list.append(random.uniform(0,2*r))
+        y_list.append(random.uniform(0,2*r))
+        if y_list[i]<sqrt(r*r-(x_list[i]-r)*(x_list[i]-r))+r and y_list[i]>-1*(sqrt(r*r-(x_list[i]-r)*(x_list[i]-r)))+r:
+            # print(x_list[i],y_list[i])
+            counter+=1
         i+=1
 
     print(background)
     print(area)
-    print(x_list)
-    print(y_list)
+    procent= counter/accuracy
+    area_1=procent*background
+    print(area_1)
 
-integral_circle(4,100)
+integral_circle(4,100000)
