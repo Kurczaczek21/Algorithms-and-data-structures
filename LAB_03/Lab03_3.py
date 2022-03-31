34fdc7f0020e1cb8803a1fe74742d3309d9ec095
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-03-31 19:26:45 +0200

    task 3.2 almost done

--- LAB_03/Lab03_3.py
+++ LAB_03/Lab03_3.py
@@ -3,7 +3,6 @@ import random
 
 def integral_circle(r, accuracy):
     background=r*r*4
-    area=3.141592*r*r
     x_list=[]
     y_list=[]
     counter=0
@@ -11,14 +10,23 @@ def integral_circle(r, accuracy):
         x_list.append(random.uniform(0,2*r))
         y_list.append(random.uniform(0,2*r))
         if y_list[i]<sqrt(r*r-(x_list[i]-r)*(x_list[i]-r))+r and y_list[i]>-1*(sqrt(r*r-(x_list[i]-r)*(x_list[i]-r)))+r:
-            # print(x_list[i],y_list[i])
             counter+=1
         i+=1
+    
+    return (counter/accuracy)*background
 
-    print(background)
-    print(area)
-    procent= counter/accuracy
-    area_1=procent*background
-    print(area_1)
+print(integral_circle(4,100000))
 
-integral_circle(4,100000)
+def integral_sin(accuracy):
+    x_list=[]
+    y_list=[]
+    counter=0
+    for i in range(accuracy):
+        x_list.append(random.uniform(0,2))
+        y_list.append(random.uniform(0,1))
+        if y_list[i]<sin(x_list[i]):
+            counter+=1
+        i+=1
+    return (counter/accuracy)*2
+
+print(integral_sin(10000))
