3b6d0909fcb4626dc46f4d09ac00dd8772be1baf
Mateusz Warzecha PC <warzecha.mati@gmail.com>
2022-04-08 00:05:05 +0200

    hanoi alg partly done

---
+++ LAB_04/Lab_04_1.py
@@ -0,0 +1,20 @@
+def Hanoi(n, sour, dest, buff): #2^n-1 dla 3 -> 7; dla 4 ->15 itd
+    print(sour,buff,dest)
+    if n==1:
+        sour-=1
+        dest+=1
+    else:
+        Hanoi(n-1,sour,buff,dest)
+        sour-=1
+        dest+=1
+        Hanoi(n-1,buff,dest,sour)
+
+Hanoi(3,3,0,0)
+
+
+
+
+    # if n%2 ==0:
+    #     #AB AC BC
+    # else:
+    #     #AC AB BC
