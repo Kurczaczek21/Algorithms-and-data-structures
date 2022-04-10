8ed7b7f1c700a4ff5646838b621f9c5fa8846688
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-04-10 15:17:04 +0200

    task 2 sth done

--- LAB_04/sort1.py
+++ LAB_04/sort1.py
@@ -1,6 +1,9 @@
 
 # ZADANIE 1
 
+from re import A
+
+
 def insertionsort(A):
     for i in range(len(A)):
         x=A[i]
@@ -13,8 +16,17 @@ def insertionsort(A):
 
 list=[5,3,1,24,54,3,54,2,3,87,43]
 print(list)
-insertionsort(list)
+# insertionsort(list)
 print(list)
 
 # ZADANIE 2
 
+def mergesort(A, a, b):
+    if a<b:
+        c=(a+b)/2
+        mergesort(A, a, c)
+        mergesort(A, c+1, b)
+        merge(T, a, c , b)
+    return A
+
+mergesort(A)
