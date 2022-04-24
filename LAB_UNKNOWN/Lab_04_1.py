bd19031ba8fcf13c9f2624aa2489e0275ecbfb38
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-04-24 13:49:36 +0200

    sth more

--- LAB_UNKNOWN/Lab_04_1.py
+++ LAB_UNKNOWN/Lab_04_1.py
@@ -3,21 +3,26 @@ def Hanoi(n, sour, dest, buff): #2^n-1 dla 3 -> 7; dla 4 ->15 itd
     if n==1:
         sour-=1
         dest+=1
-    Hanoi(n-1,sour,buff,dest)
-    sour-=1
-    dest+=1
-    Hanoi(n-1,buff,dest,sour)
+    else:
+        Hanoi(n-1,sour,buff,dest)
+        sour-=1
+        dest+=1
+        Hanoi(n-1,buff,dest,sour)
 
-Hanoi(3,3,0,0)
+Hanoi(4,4,0,0)
 
 def Hanoi_2(n, sour, dest, buff):
-    WHILE (sour != Null OR buff !=Null):
-        IF i%3 == 1:
+    i=0
+    while sour != None or buff !=None:
+        if i%3 == 1:
             Possible move disk between sour and dest
-        IF i%3 == 2:
+            i+=1
+        if i%3 == 2:
             Possible move disk between sour and buff
-        IF i%3 == 0:
+            i+=1
+        if i%3 == 0:
             Possible move disk between buff and dest
+            i+=1
 
 
     # if n%2 ==0:
