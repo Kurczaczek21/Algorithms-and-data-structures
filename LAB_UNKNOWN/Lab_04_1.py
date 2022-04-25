dbed198ddc56397dec2df1c68c9ca7e38726e92c
Mateusz Warzecha PC <warzecha.mati@gmail.com>
2022-04-25 22:01:08 +0200

    task 2 done ; F for optimalization

--- LAB_UNKNOWN/Lab_04_1.py
+++ LAB_UNKNOWN/Lab_04_1.py
@@ -10,23 +10,83 @@ def Hanoi_req(n: int, sour: str, dest:str, buff:str)->None: #2^n-1 dla 3 -> 7; d
         print('move from:'+str(sour)+' to '+str(dest))
         Hanoi_req(n-1,buff,dest,sour)
 
-Hanoi_req(3,'sour','buff','dest')
-
-# def possible_move(x: str, y:str):
-
+def make_hanoi(n:int):
+    hanoi=[]
+    for i in range(n):
+        hanoi.append(n-i)
+    return hanoi
 
 def Hanoi_2(n:int, sour:str, dest:str, buff:str)->None:
+    a=make_hanoi(n) #sour
+    b=[]            #buff
+    c=[]            #dest
     for i in range(1,2**n):
+        print(i)
         if i%3 == 1:
-            print('Possible move disk between '+sour+' and '+dest)
+            print(a,b,c)
+            if len(c)==0 and len(a)!=0:
+                c.append(a[len(a)-1])
+                a.remove(a[len(a)-1])
+                print('dadad')
+                print('Move disk from '+sour+' to '+dest)
+            elif len(a)==0 and len(c)!=0:
+                a.append(c[len(c)-1])
+                c.remove(c[len(c)-1])
+                print('Move disk from '+dest+' to '+sour)
+            elif a[len(a)-1]<c[len(c)-1]:
+                c.append(a[len(a)-1])
+                a.remove(a[len(a)-1])
+                print(a,b,c)
+                print('Move disk from '+sour+' to '+dest)
+            else:
+                a.append(c[len(c)-1])
+                c.remove(c[len(c)-1])
+                print('Move disk from '+dest+' to '+sour)
+            if not a and not b:
+                    break
+                
         if i%3 == 2:
-            print('Possible move disk between '+sour+' and '+buff)
+            print(a,b,c)
+            if len(b)==0 and len(a)!=0:
+                b.append(a[len(a)-1])
+                a.remove(a[len(a)-1])
+                print('Move disk from '+sour+' to '+buff)
+            elif len(a)==0 and len(b)!=0:
+                a.append(b[len(b)-1])
+                b.remove(b[len(b)-1])
+                print('Move disk from '+buff+' to '+sour)
+            elif a[len(a)-1]<b[len(b)-1]:
+                b.append(a[len(a)-1])
+                a.remove(a[len(a)-1])
+                print('Move disk from '+sour+' to '+buff)
+            else:
+                a.append(b[len(b)-1])
+                b.remove(b[len(b)-1])
+                print('Move disk from '+buff+' to '+sour)
+            if not a and not b:
+                    break
+                
         if i%3 == 0:
-            print('Possible move disk between '+buff+' and '+dest)
+            print(a,b,c)
+            if len(b)==0 and len(c)!=0:
+                b.append(c[len(c)-1])
+                c.remove(c[len(c)-1])
+                print('Move disk from '+dest+' to '+buff)
+            elif len(c)==0 and len(b)!=0:
+                c.append(b[len(b)-1])
+                b.remove(b[len(b)-1])
+                print('Move disk from '+buff+' to '+dest)
+            elif c[len(c)-1]<b[len(b)-1]:
+                b.append(c[len(c)-1])
+                c.remove(c[len(c)-1])
+                print('Move disk from '+dest+' to '+buff)
+            else:
+                c.append(b[len(b)-1])
+                b.remove(b[len(b)-1])
+                print('Move disk from '+buff+' to '+dest)
+            if not a and not b:
+                    break
 
-Hanoi_2(3,'sour','buff','dest')
+Hanoi_req(3,'sour','dest','buff')
 
-    # if n%2 ==0:
-    #     #AB AC BC
-    # else:
-    #     #AC AB BC
+Hanoi_2(3,'sour','dest','buff')
