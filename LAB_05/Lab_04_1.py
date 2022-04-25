1919b26c19f1161ce336b92fb86ca89bc22134fd
Mateusz Warzecha PC <warzecha.mati@gmail.com>
2022-04-25 23:29:46 +0200

    alb 4 100% done

--- LAB_05/Lab_04_1.py
+++ LAB_05/Lab_04_1.py
@@ -1,13 +1,12 @@
+from time import time
 
 def Hanoi_req(n: int, sour: str, dest:str, buff:str)->None:
     '''
     hanoi recursive solve function
     '''
-    if n==1:
-        print('move from:'+str(sour)+' to '+str(dest))
-    else:
+    if n>0:
         Hanoi_req(n-1,sour,buff,dest)
-        print('move from:'+str(sour)+' to '+str(dest))
+        # print('move from:'+str(sour)+' to '+str(dest))
         Hanoi_req(n-1,buff,dest,sour)
 
 def make_hanoi(n:int):
@@ -25,11 +24,11 @@ def list_change(a:str,b:str,x:list,y:list)->None:
     '''
     y.append(x[len(x)-1])
     x.remove(x[len(x)-1])
-    print('Move disk from '+a+' to '+b)
+    # print('Move disk from '+a+' to '+b)
 
 def possible_move(a:str,b:str,x:list,y:list)->None:
     '''
-    checks wich move could be made
+    checks which move could be made
     '''
     if len(y)==0:
         list_change(a,b,x,y)
@@ -63,6 +62,25 @@ def Hanoi_2(n:int, sour:str, dest:str, buff:str)->None:
             if not a and not b:
                 break
 
-Hanoi_req(3,'sour','dest','buff')
-
-Hanoi_2(3,'sour','dest','buff')
+if __name__ == '__main__':
+    
+    total_start=time()
+    
+    hanoi_req_time=[]
+    hanoi_iter_time=[]
+    
+    for i in range(1,30):
+    # Hanoi recursive solve time: 198.02846217155457        for n=30
+    # Hanoi iteration solve time:842.4369783401489
+    # Total time: 1040.4659361839294
+        start=time()
+        Hanoi_req(i,'sour','dest','buff')
+        hanoi_req_time.append(time()-start)
+        
+        start=time()
+        Hanoi_2(i,'sour','dest','buff')
+        hanoi_iter_time.append(time()-start)
+    
+    print('Hanoi recursive solve time: ' + str(sum(hanoi_req_time)))
+    print('Hanoi iteration solve time:' + str(sum(hanoi_iter_time)))
+    print('Total time: '+ str(time()-total_start))
