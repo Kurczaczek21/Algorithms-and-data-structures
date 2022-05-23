6dc62f556a060257792cd0f5b562b4ac863e9111
Mateusz Warzecha PC <warzecha.mati@gmail.com>
2022-05-23 21:49:00 +0200

    sth done

--- LAB_08/Lab_08_01.py
+++ LAB_08/Lab_08_01.py
@@ -24,30 +24,35 @@ def pathfinder_naive(source:int,coordinates:list):
     print(cities)
     return sum_path
 
-def find_closest_val(core:float,elements:list):
+def search_min_distances(elements:list, core:int):
+    min_distance=[1000,0]
     for i in range(len(elements)):
-        if elements[i]==min(elements, key=lambda x:abs(x-core)):
-            return i
-    # return min(elements, key=lambda x:abs(x-core))
-
-def pathfinder_optimize(source:int,coordinates:list):
-    sum_path=0
-    x_coordinates=[]
-    y_coordinates=[]
-    for i in range(len(coordinates)):
-        if i== source-1:
-            x_coordinates.append(1000)
-            y_coordinates.append(1000)
-        else:    
-            x_coordinates.append(coordinates[i][1])
-            y_coordinates.append(coordinates[i][2])
+        distance=sqrt(((elements[int((core+i)%len(elements))][1]-elements[int((core+i+1)%len(elements))][1]))**2+(elements[int((core+i)%len(elements))][2]-elements[int((core+i+1)%len(elements))][2])**2)
+        if distance<min_distance[0] and i!=core-1:
+            min_distance[0]=distance
+            min_distance[1]=elements[int((core+i)%len(elements))][0]
+    return min_distance
     
 
-
+def pathfinder_optimize(source:int,coordinates:list):
+    
+    coordinates.pop(source-1)
+    temp=search_min_distances(coordinates,source)
+    coordinates.pop(source-1)
+    sum_path=temp[0]
+    path_id=[temp[1]]
+    for i in range(len(coordinates)-1):
+        point=search_min_distances(coordinates,temp[1])
+        if  coordinates[i][0]==int(point[1]):
+            path_id.append(point[1])
+            coordinates.pop(int(point[1]-1))
+        sum_path+=point[0]
+    print(path_id)
+    return sum_path
 
 
 if __name__=='__main__':
-    x=pathfinder_naive(1,open_file())
+    x=pathfinder_optimize(1,open_file())
     print(x)
 
 
