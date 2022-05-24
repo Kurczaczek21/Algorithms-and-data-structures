88d30e3bf8481d8f1e364473e0e7bb2790812b92
Mateusz Warzecha PC <warzecha.mati@gmail.com>
2022-05-24 12:19:56 +0200

    almost done

--- LAB_08/Lab_08_01.py
+++ LAB_08/Lab_08_01.py
@@ -33,36 +33,31 @@ def search_min_distances(elements:list, core:int): #core=index+1 ; index=core-1
                if elements[j][0]==core:
                     temp_id=j    #element tablicy = temp id
             distance=sqrt(((elements[temp_id][1]-elements[i][1]))**2+(elements[temp_id][2]-elements[i][2])**2)
-            print('DISTANCE')
-            print(distance)
-            print(core,elements[i][0])
             if distance<min_distance[0]:
-                
-                print(elements[i][0])
                 min_distance[0]=distance
                 min_distance[1]=elements[i][0]
     return min_distance
     
 
 def pathfinder_optimize(source:int,coordinates:list): #source = index +1 ; index=source-1
-    
     temp=search_min_distances(coordinates,source)
     coordinates.pop(source-1)
     sum_path=temp[0]
     path_id=[temp[1]]
-    print(sum_path)
+    for i in range(len(coordinates)):
+        point=search_min_distances(coordinates,temp[1])
+        path_id.append(point[1])
+        for j in range(len(coordinates)):
+            if coordinates[j][0]==point[1]:
+                coordinates.pop(j)
+                break
+        sum_path+=point[0]
     print(path_id)
-    # for i in range(len(coordinates)-1):
-    #     point=search_min_distances(coordinates,temp[1])
-    #     if  coordinates[i][0]==int(point[1]):
-    #         path_id.append(point[1])
-    #         coordinates.pop(int(point[1]-1))
-    #     sum_path+=point[0]
     return sum_path
 
 
 if __name__=='__main__':
-    x=pathfinder_optimize(2,open_file())
+    x=pathfinder_optimize(64,open_file())
     print(x)
 
 
