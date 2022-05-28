370ba1d91abea85c78cc2b8e3c378c17523292b8
Mateusz Warzecha <warzecha.mati@gmail.com>
2022-05-28 11:29:37 +0200

    objects inserted

--- LAB_09/Lab_09_01.py
+++ LAB_09/Lab_09_01.py
@@ -1,11 +1,15 @@
 class Item:
-    def __init__(self, width,height,value) -> None:
+    def __init__(self,id:int, width:int,height:int,value:int) -> None:
+        self.id=id
         self.width=width
         self.height=height
         self.value=value
 
     def surface_area(self):
         return self.width*self.height
+
+    def get_id(self):
+        return self.id
     
     def get_value(self):
         return self.value
@@ -16,13 +20,24 @@ class Item:
         self.height=x
     
     def printer(self):
-        print(self.value,self.width,self.height)
+        print(self.id,self.width,self.height,self.value)
 
     def proportion(self):
         return self.value/(self.width*self.height)
 
-pan=Item(1,30,69)
-pan.printer()
-pan.rotate()
-pan.printer()
-print(pan.proportion())
+def open_file(data:int):
+    """opens file with items
+    """
+    file=open('C:\PY prog\LAB_09\packages\packages'+str(data)+'.txt','r').readlines()
+    items=[]
+    file.pop(0) 
+    file.pop(0)
+    for i in range(len(file)):    
+        file[i]=file[i].split(',')
+        items.append(Item(int(file[i][0]),int(file[i][1]),int(file[i][2]),int(file[i][3])))
+
+    return items
+
+items=open_file(20)
+items[0].printer()
+print(items[1].get_value())
